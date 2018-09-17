0c069a07de2f31e817dff9dd906c1a6bc952afbf
Waaamsley <walja829@student.otago.ac.nz>
2018-09-17 15:24:14 +1200

    Add files via upload

--- qNavigation.py
+++ qNavigation.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2
+import random
 import navigation
 import communicator
 import sys
 import os
 import pickle
 import time
+import math
 from gridworld import gridworld
 
 alpha = 0.05
@@ -14,6 +16,13 @@ navi = navigation.navigator()
 comms = communicator.client()
 start_time = time.time()
 
+# navi.pre_re_align()
+# with open(os.path.dirname(__file__) + '/Qbase_dumb.data', 'rb') as f:
+#     f.seek(0)
+#     Q = pickle.load(f)
+#     f.close()
+# print(Q)
+# quit()
 def move_robot(origin, target, state):
     if origin == target:
         return state
@@ -33,6 +42,33 @@ def move_robot(origin, target, state):
     # print(section, origin, target)
     navi.self_navigate(comms, section, True)
     return section
+
+
+def get_action(values):
+    results = []
+    probs = []
+    gamma = 2
+    sum = 0
+
+    for item in values:
+        result = math.exp(gamma * item)
+        results.append(result)
+        sum += result
+
+    for item in results:
+        prob = item/sum
+        probs.append(prob)
+
+    x = random.uniform(0, 1)
+    cumulative_prob = 0.0
+    index = None
+    for i in range(len(results)):
+        cumulative_prob += probs[i]
+        if x < cumulative_prob:
+            index = i
+            break
+    return index
+
 
 
 navigated = navi.self_navigate(comms, "00", False)
@@ -71,16 +107,16 @@ for key in env_keys:
 print(robot_env)
 env = gridworld(robot_env)
 
-# Q = np.random.uniform(0, 0, (15, 4))
-# Q[14] = [1, 1, 1, 1]
-# with open(os.path.dirname(__file__) + '/Qbase_smart.data', 'wb') as f:
-#     pickle.dump(Q, f)
+Q = np.random.uniform(0, 0, (15, 4))
+Q[14] = [1, 1, 1, 1]
+with open(os.path.dirname(__file__) + '/Qbaseprob_dumb.data', 'wb') as f:
+    pickle.dump(Q, f)
 Q = None
-with open(os.path.dirname(__file__) + '/Qbase_smart.data', 'rb') as f:
+with open(os.path.dirname(__file__) + '/Qbaseprob_dumb.data', 'rb') as f:
     f.seek(0)
     Q = pickle.load(f)
     f.close()
-# print(Q)
+print(Q)
 
 learning = True
 iteration = 1
@@ -88,39 +124,39 @@ fails = 0
 while(learning and Q is not None):
     print("Iteration: ", iteration)
     print("--- %s seconds ---" % (time.time() - start_time))
-    seen = []
+    #seen = []
     state, reward = env.reset(robot_state)
     new_state = None
     position = "00"
 
     while (env.terminal() == False):
-        action = np.argmax(Q[state,:])
+        action = get_action(Q[state])#np.argmax(Q[state,:])
         new_state, reward = env.step(action)
         position = move_robot(state, new_state, position)
         # print(state, action, new_state)
 
-        if new_state in seen:
-            reward = -0.5
-        else:
-            seen.append(new_state)
+        # if new_state in seen:
+        #     reward = -0.5
+        # else:
+        #     seen.append(new_state)
         target_score = reward + np.max(Q[new_state])
-        Q[state, action] = Q[state, action] + (alpha*(target_score - Q[state, action]))
-
+        Q[state, action] = Q[state, action] + (alpha * (target_score - Q[state, action]))
         if reward == -0.9:
             fails += 1
+            print("Fail")
             break
+
         state = new_state
 
     # print(Q)
     if iteration % 10 == 0:
         print("Successses: ", 10 - fails)
         fails = 0
-    with open(os.path.dirname(__file__) + '/Qbase_smart.data', 'wb') as f:
+    with open(os.path.dirname(__file__) + '/Qbaseprob_dumb.data', 'wb') as f:
         pickle.dump(Q, f)
     navi.self_navigate(comms, "00", False)
     navi.pre_re_align()
     iteration += 1
 
-    #learning = False
 
 comms.close_connection()
